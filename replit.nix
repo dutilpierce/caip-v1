a93d57dba9fc5742a5e4d703a1579c420db15881
Your Name <you@quarterpay.com>
2026-02-03 10:18:23 -0600

    Fix replit.nix syntax

--- replit.nix
+++ replit.nix
@@ -4,13 +4,3 @@
     pkgs.nodejs_18
   ];
 }
-{ pkgs }: {
-  deps = [
-    pkgs.python3
-    pkgs.python3Packages.httpx
-    pkgs.python3Packages.supabase
-    pkgs.python3Packages.gotrue
-    pkgs.python3Packages.python-dotenv
-    pkgs.nodejs_18
-  ];
-}
